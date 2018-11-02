1291f0d5481e0f84d32f0d78082c41c8a6ca512d
xulifeng <1051068352@qq.com>
2018-11-02 14:19:58 +0800

    新增通过spring配置文件生成代理对象调用远程服务

--- src/main/java/com/beidahuang/main/ServerMain.java
+++ src/main/java/com/beidahuang/main/ServerMain.java
@@ -3,11 +3,11 @@ package com.beidahuang.main;
 import com.beidahuang.hello.Hello;
 import com.beidahuang.hello.impl.HelloImpl;
 import com.beidahuang.register.impl.RegisterImpl;
-import com.beidahuang.rpcServer.RpcServer;
 import com.beidahuang.user.User;
 import com.beidahuang.user.impl.UserImpl;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooKeeper;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
 
@@ -15,17 +15,9 @@ public class ServerMain {
 
     public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
 
+        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext(new String[] {"META-INF/dubbo-provider.xml"});
 
-        Hello hello = new HelloImpl();
-        User user = new UserImpl();
-        //发布接口
 
-        RegisterImpl register = new RegisterImpl();
-
-        RpcServer rpcServer = new RpcServer(register,"localhost:8080");
-
-        rpcServer.bind(hello,user);
-        rpcServer.registetrAndListener();
 
     }
 }
